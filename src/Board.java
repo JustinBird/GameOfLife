b888fcaafcd85002c069576f90492a6845afa72f
Justin <justinbird1999@gmail.com>
2019-05-15 23:13:37 -0500

    Wrote function to count neighbors.

--- src/Board.java
+++ src/Board.java
@@ -19,6 +19,11 @@ public class Board extends JPanel implements MouseListener {
         return boardWidth;
     }
 
+    /**
+     * Board constructor. Initializes class variables and adds mouse listener.
+     * @param width Width of board.
+     * @param height Height of board.
+     */
     public Board(int width, int height) {
         this.boardWidth = width;
         this.boardHeight = height;
@@ -27,10 +32,18 @@ public class Board extends JPanel implements MouseListener {
         addMouseListener(this);
     }
 
+    /**
+     * Flips a position on the board.
+     * @param x X Position.
+     * @param y Y position.
+     */
     public void setBoard(int x, int y) {
         board[y][x] = !board[y][x];
     }
 
+    /**
+     * Debug function.
+     */
     public void printBoard() {
         for (int i = 0; i < boardHeight; i++) {
             for (int j = 0; j < boardWidth; j++) {
@@ -45,6 +58,10 @@ public class Board extends JPanel implements MouseListener {
         return new Dimension(boardWidth * 10, boardHeight * 10);
     }
 
+    /**
+     * Draws board as a JPanel.
+     * @param g Graphics object.
+     */
     @Override
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
@@ -65,6 +82,7 @@ public class Board extends JPanel implements MouseListener {
     @Override
     public void mousePressed(MouseEvent e) {
         setBoard(e.getX() / 10, e.getY() / 10);
+        System.out.println(countNeighbors(e.getX() / 10, e.getY() / 10));
         repaint();
     }
 
@@ -84,5 +102,35 @@ public class Board extends JPanel implements MouseListener {
     @Override
     public void mouseClicked(MouseEvent e) {
 
+    }
+
+    /**
+     * Counts the number of neighbors a square has. Contributions from:
+     * https://codereview.stackexchange.com/questions/62160/checking-for-neighbours-more-elegantly-in-conways-game-of-life
+     * @param x X position.
+     * @param y Y position.
+     * @return Number of neighbors.
+     */
+    public int countNeighbors(int x , int y) {
+
+        int[][] neighborOffset = {
+                {-1, -1}, {0, -1}, {1, -1},
+                {-1,  0},          {1,  0},
+                {-1,  1}, {0,  1}, {1,  1}
+        };
+
+        int neighbors = 0;
+
+        for (int[] offset : neighborOffset) {
+            if (x + offset[0] >= 0 && x + offset[0] < boardWidth) {
+                if (y + offset[1] >= 0 && y + offset[1] < boardHeight) {
+                    if (board[y + offset[1]][x + offset[0]]) {
+                        neighbors += 1;
+                    }
+                }
+            }
+        }
+
+        return neighbors;
     }
 }
