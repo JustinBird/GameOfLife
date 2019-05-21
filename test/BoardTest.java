0e129e02c2079b7ce3f2d6083538873c5e2abdfd
Justin <justinbird1999@gmail.com>
2019-05-20 19:21:57 -0500

    Added board tests.

---
+++ test/BoardTest.java
@@ -0,0 +1,37 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class BoardTest {
+
+    /**
+     * Assert all squares have zero neighbors on an empty board.
+     */
+    @Test
+    public void CountNeighborsEmpty() {
+
+        Board b = new Board(5, 5);
+
+        for (int i = 0; i < b.getBoardHeight(); i++) {
+            for (int j = 0; j < b.getBoardWidth(); j++) {
+                assertEquals(b.countNeighbors(j, i), 0);
+            }
+        }
+    }
+
+    /**
+     * Assert a center square has eight neighbors on a full board.
+     */
+    @Test
+    public void CounterNeighborsFull() {
+
+        Board b = new Board(5, 5);
+        for (int i = 0; i < b.getBoardHeight(); i++) {
+            for (int j = 0; j < b.getBoardWidth(); j++) {
+                b.setBoard(i, j);
+            }
+        }
+
+        assertEquals(b.countNeighbors(2, 2), 8);
+    }
+}
